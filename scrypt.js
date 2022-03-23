5e93aae734ac6b46ab3eb475869e62c22ec4dda7
Brunolops <brunooliveira99874@hotmail.com>
2022-03-22 22:21:23 -0300

    trabalhando com distancias

--- scrypt.js
+++ scrypt.js
@@ -1,13 +1,25 @@
-// Adicione a classe ativo a todos os itens do menu
-// Remove a classe ativo de todos os itens do menu e mantenha ape
-// Verifique se as imagens possuem o atributo alt
-// Modifique o href do link externo no menu
+// Retorne a soma da largura de todas as imagens
 
-const menu = document.querySelector ('.menu');
-console.log (menu);
 
-menu.setAttribute ('href', 'ativo');
 
-const img = document.querySelector('img');
+// Verifique se os links da página possuem
+// o mínimo recomendado para telas utilizadas
+// com o dedo. (48px/48px de acordo com o google)
+// Se o browser for menor que 720px,
+// adicione a classe menu-mobile ao menu
 
-img.hasAttribute ('alt')
+
+
+const dados = document.querySelectorAll ('a')
+console.log (dados);
+
+dados.forEach ((google) => {
+  const linkWidth = google.offsetWidth
+  const linkHeight = google.offsetHeight
+  if (linkHeight >= 48 && linkWidth >= 48) {
+    console.log (google,'ok google')
+  } else {
+    console.log (google,'nao ok google')
+  }
+})
+
